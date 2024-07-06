9143c20eaf8e691671d40c59ae77be117a02d773
Dhruvraj Singh <64192503+dhruvraj508@users.noreply.github.com>
2024-07-06 12:29:00 -0600

    added the incomplete header frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,16 +6,24 @@ import './App.css'
 function App() {
 
   return (
-
-
     <div>
-      <header>
+      <header className='p-4 flex justify-between'>
         <a href="" className="flex items-center gap-1">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-6 -rotate-90">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-6">
           <path strokeLinecap="round" strokeLinejoin="round" d="M12 21v-8.25M15.75 21v-8.25M8.25 21v-8.25M3 9l9-6 9 6m-1.5 12V10.332A48.36 48.36 0 0 0 12 9.75c-2.551 0-5.056.2-7.5.582V21M3 21h18M12 6.75h.008v.008H12V6.75Z" />
         </svg>
-        <span className='fot-bold'>airbnb</span>
+        <span className='font-bold text-xl'>cloudbnb</span>
         </a>
+        <div className='flex border border-gray-300 rounded-full py-2 px-4'>
+          <div>Anywhere</div>
+          <div>Any Week</div>
+          <div>Add Guests</div>
+          <button>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+            </svg>
+          </button>
+        </div>
       </header>
     </div>
   )
