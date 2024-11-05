a5341243c301a799e5d8a63740d8eacc942c8dea
Dhruvraj Singh <64192503+dhruvraj508@users.noreply.github.com>
2024-11-05 16:38:17 -0700

    minor tweaks

--- client/src/BookingComponent.jsx
+++ client/src/BookingComponent.jsx
@@ -1,4 +1,17 @@
+import React, {useState} from 'react';
+import {differenceInCalendarDays} from 'date-fns'
+import {UserContext} from './UserContext';
+import {useContext} from 'react';
+
 export default function BookingComponent({place}){
+    const [checkInTime, setCheckInTime] = useState('');
+    const [checkOutTime, setCheckOutTime] = useState('');
+    const [numberOfGuests, setNumberOfGuests] = useState(1);
+    const { user, ready } = useContext(UserContext);
+    let days = 0;
+    if (checkInTime && checkOutTime){
+        days = differenceInCalendarDays(new Date(checkOutTime), new Date(checkInTime));
+    }
     return (
         <div className="bg-white border shadow-lg p-4 rounded-2xl mt-4">
                     <div className="text-2xl text-center">
@@ -8,19 +21,39 @@ export default function BookingComponent({place}){
                         <div className="flex">
                             <div className="py-3 px-4 ">
                                 <label>Check in:</label>
-                                <input type="date" />
+                                <input type="date" 
+                                        value={checkInTime} 
+                                        onChange={ev=>setCheckInTime(ev.target.value)}/>
                             </div>
                             <div className="py-3 px-4 border-l">
                                 <label>Check out:</label>
-                                <input type="date" />
+                                <input type="date" 
+                                        value={checkOutTime} 
+                                        onChange={ev=>setCheckOutTime(ev.target.value)}/>
                             </div>
                         </div>
                         <div className="py-3 px-4 border-t">
                             <label>Guests:</label>
-                            <input type="number" value={1} />
+                            <input type="number" 
+                                    value={numberOfGuests} 
+                                    onChange={ev=>setNumberOfGuests(ev.target.value)}/>
                         </div>
+                        {days >0 && !!user &&(
+                            <div className="py-3 px-4 border-t">
+                                <label>Name:</label>
+                                <input type="Text" 
+                                        placeholder={user.name}/>
+                        </div>
+                        )}
                     </div>
-                    <button className="primary mt-4">Reserve</button>
+                    <button className="primary mt-4">
+                        Reserve for   
+                        {days > 0 && (
+                            <span className='mx-1'>
+                                ${days * place.price} 
+                            </span>
+                        )}
+                    </button>
                 </div>
     );
 }
--- client/src/pages/ListingPage.jsx
+++ client/src/pages/ListingPage.jsx
@@ -24,23 +24,7 @@ export default function ListingPage() {
 
     if (showAllPhotos){
         return (
-            // <div className="absolute inset-0 bg-white min-h-screen">
-            //     <div className="p-8 grid gap-4">
-            //         <div>
-            //         <button className="flex fixed gap-1 py-2 px-4 rounded-2xl bg-gray-300 shadow shadow-gray-500 shadow-black">
-            //             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
-            //             <path stroke-linecap="round" stroke-linejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-            //             </svg>
-            //             Close Photos
-            //         </button>
-            //     </div>
-            //         {place?.photos?.length > 0 && place.photos.map(photo => (
-            //             <div>
-            //                 <img className="w-full" src={"http://localhost:4000/uploads/"+photo} alt="" />
-            //             </div>
-            //         ))}
-            // </div>
-            // </div>
+            
             <div className="absolute inset-0 bg-white min-h-screen p-8">
                 <div className="relative">
                     <button
@@ -81,48 +65,7 @@ export default function ListingPage() {
     }
 
     return (
-        // <div className="text-left mt-4  -mx-8 px-8 py-4">
-        //     <h1 className="text-2xl">{place.title}</h1>
-        //     <a className="my-2 mb-2 block font-semibold underline" target="_blank" href={'https://maps.google.com/?q='+place.address} >{place.address}</a>
-        //     <div className="relative">
-        //         <div className="grid gap-2 grid-cols-[2fr_1fr_1fr] ">
-        //             <div>
-        //                 {place.photos?.[0] &&(
-        //                     <div>
-        //                         <img className="aspect-square object-cover" src={"http://localhost:4000/uploads/"+place.photos[0]} alt="" />
-        //                     </div>
-        //                 )}
-        //             </div>
-        //             <div className="grid">
-        //                 {place.photos?.[1] &&(
-        //                     <img className="aspect-square object-cover" src={"http://localhost:4000/uploads/"+place.photos[1]} alt="" />
-        //                 )}
-        //                 <div className="overflow-hidden">
-        //                     {place.photos?.[2] &&(
-        //                     <img className="aspect-square object-cover relative top-2" src={"http://localhost:4000/uploads/"+place.photos[2]} alt="" />
-        //                     )}
-        //                 </div>
-        //             </div>
-        //             <div className="grid">
-        //                 {place.photos?.[3] &&(
-        //                     <img className="aspect-square object-cover" src={"http://localhost:4000/uploads/"+place.photos[3]} alt="" />
-        //                 )}
-        //                 <div className="overflow-hidden">
-        //                     {place.photos?.[4] &&(
-        //                     <img className="aspect-square object-cover relative top-2" src={"http://localhost:4000/uploads/"+place.photos[4]} alt="" />
-        //                     )}
-        //                 </div>
-        //             </div>
-        //         </div>
-        //         <button onClick={()=>setShowAllPhotos(true)} className="flex gap-1 absolute bottom-2 right-2 py-1 px-4 bg-white rounded-lg border border-black">
-        //             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
-        //             <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 7.125C2.25 6.504 2.754 6 3.375 6h6c.621 0 1.125.504 1.125 1.125v3.75c0 .621-.504 1.125-1.125 1.125h-6a1.125 1.125 0 0 1-1.125-1.125v-3.75ZM14.25 8.625c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v8.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 0 1-1.125-1.125v-8.25ZM3.75 16.125c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v2.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 0 1-1.125-1.125v-2.25Z" />
-        //             </svg>
-        //             Show All Photos
-        //         </button>
-        //     </div>
-            
-        // </div>
+        
         <div className="text-left mt-4 -mx-8 px-8 py-4">
         <h1 className="text-2xl font-semibold">{place.title}</h1>
         <a
@@ -144,7 +87,8 @@ export default function ListingPage() {
                 <div>
                     {place.photos?.[0] && (
                         <img
-                            className="aspect-square object-cover"
+                            onClick={() => setShowAllPhotos(true)}
+                            className="aspect-square object-cover cursor-pointer"
                             src={"http://localhost:4000/uploads/" + place.photos[0]}
                             alt=""
                         />
@@ -155,7 +99,8 @@ export default function ListingPage() {
                 <div className="grid">
                     {place.photos?.[1] && (
                         <img
-                            className="aspect-square object-cover"
+                            onClick={() => setShowAllPhotos(true)}
+                            className="aspect-square object-cover cursor-pointer"
                             src={"http://localhost:4000/uploads/" + place.photos[1]}
                             alt=""
                         />
@@ -163,7 +108,8 @@ export default function ListingPage() {
                     <div className="overflow-hidden">
                         {place.photos?.[2] && (
                             <img
-                                className="aspect-square object-cover relative top-2"
+                                onClick={() => setShowAllPhotos(true)}  
+                                className="aspect-square object-cover relative top-2 cursor-pointer"
                                 src={"http://localhost:4000/uploads/" + place.photos[2]}
                                 alt=""
                             />
@@ -175,7 +121,8 @@ export default function ListingPage() {
                 <div className="grid">
                     {place.photos?.[3] && (
                         <img
-                            className="aspect-square object-cover"
+                            onClick={() => setShowAllPhotos(true)}
+                            className="aspect-square object-cover cursor-pointer"
                             src={"http://localhost:4000/uploads/" + place.photos[3]}
                             alt=""
                         />
@@ -183,7 +130,8 @@ export default function ListingPage() {
                     <div className="overflow-hidden">
                         {place.photos?.[4] && (
                             <img
-                                className="aspect-square object-cover relative top-2"
+                                onClick={() => setShowAllPhotos(true)}
+                                className="aspect-square object-cover relative top-2 cursor-pointer"
                                 src={"http://localhost:4000/uploads/" + place.photos[4]}
                                 alt=""
                             />
@@ -230,15 +178,15 @@ export default function ListingPage() {
                 <BookingComponent place={place}/>
             </div>
         </div>
-        <div>
-
+        <div className="border-t -mx-8 px-8 py-8">
+            <div>
+                <h2 className=" font-semibold text-2xl">Extra Information</h2>
+            </div>
+            <div className="mb-4 mt-2 text-sm text-gray-800 leading-5">
+                        {place.miscInfo}
+            </div>
         </div>
-        <div>
-            <h2 className=" font-semibold text-2xl">Extra Information</h2>
-        </div>
-        <div className="mb-4 mt-1 text-sm text-gray-800 leading-4">
-                    {place.miscInfo}
-        </div>
+        
     </div>
     );
 }
